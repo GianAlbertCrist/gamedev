e9f3283262854b354446ea9d4d485c45c7ef51c9
yurizz-crypto <pacaljoenel08@gmail.com>
2025-04-25 17:17:30 +0800

    Initialize UI, Nav Bottom Bar with Icons with Fragments of the Binded Main

---
+++ app/src/main/java/com/budgetapp/thrifty/MainActivity.java
@@ -0,0 +1,89 @@
+package com.budgetapp.thrifty;
+
+import android.content.res.ColorStateList;
+import android.os.Bundle;
+import android.view.Window;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+import androidx.core.view.WindowCompat;
+import androidx.core.view.WindowInsetsControllerCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+import android.content.res.Configuration;
+
+import com.budgetapp.thrifty.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        syncNotificationBarColor();
+        themeSync();
+
+        replaceFragment(new HomeFragment());
+        binding.bottomNav.setBackground(null);
+
+        binding.bottomNav.setOnItemSelectedListener(item -> {
+            int id = item.getItemId();
+
+            if (id == R.id.ic_home) {
+                replaceFragment(new HomeFragment());
+                return true;
+
+            } else if (id == R.id.ic_transactions) {
+                replaceFragment(new TransactionsFragment());
+                return true;
+
+            } else if (id == R.id.ic_reports) {
+                replaceFragment(new ReportsFragment());
+                return true;
+
+            } else if (id == R.id.ic_profile) {
+                replaceFragment(new ProfileFragment());
+                return true;
+
+            } else {
+                return false;
+            }
+        });
+    }
+
+    public void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout, fragment);
+        fragmentTransaction.commit();
+    }
+
+    public void syncNotificationBarColor() {
+        Window window = getWindow();
+
+        // Change status bar color to Lavender
+        window.setStatusBarColor(ContextCompat.getColor(this, R.color.primary_color)); // Lavender color
+
+        // Set the status bar icons to dark or light based on the color
+        WindowInsetsControllerCompat insetsController = WindowCompat.getInsetsController(window, window.getDecorView());
+        insetsController.setAppearanceLightStatusBars(true); // Change to false if you want light icons on a dark background
+    }
+    public void themeSync() {
+        boolean isDarkMode = (getResources().getConfiguration().uiMode
+                & Configuration.UI_MODE_NIGHT_MASK) == Configuration.UI_MODE_NIGHT_YES;
+
+        ColorStateList iconTintDay = ContextCompat.getColorStateList(this, R.color.bottom_nav_icon_selector_day);
+        ColorStateList iconTintNight = ContextCompat.getColorStateList(this, R.color.bottom_nav_icon_selector_night);
+
+        if (isDarkMode) {
+            binding.bottomNav.setItemIconTintList(iconTintNight);
+        } else {
+            binding.bottomNav.setItemIconTintList(iconTintDay);
+        }
+    }
+}
