a18722e371271e3c14746455068ae979df703501
Gian Daytic <s.daytic.gianalbert@cmu.edu.ph>
2025-05-01 20:08:42 +0800

    Report Activity Added

--- app/src/main/java/com/budgetapp/thrifty/ReportsFragment.java
+++ app/src/main/java/com/budgetapp/thrifty/ReportsFragment.java
@@ -1,17 +1,62 @@
 package com.budgetapp.thrifty;
 
 import android.os.Bundle;
-
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.charts.PieChart;
 
 public class ReportsFragment extends Fragment {
+    private PieChartManager pieChartManager;
+    private BarChartManager barChartManager;
+    private TextView tvBalanceAmount, tvTotalIncome, tvTotalExpense;
+    private float income = 0f;
+    private float expense = 0f;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_report, container, false);
+        View view = inflater.inflate(R.layout.fragment_report, container, false);
+
+        try {
+            // Initialize views
+            PieChart pieChart = view.findViewById(R.id.pieChart);
+            BarChart barChart = view.findViewById(R.id.barChart);
+            TextView trendToggle = view.findViewById(R.id.trendToggle);
+            tvBalanceAmount = view.findViewById(R.id.tvBalanceAmount);
+            tvTotalIncome = view.findViewById(R.id.tvTotalIncome);
+            tvTotalExpense = view.findViewById(R.id.tvTotalExpense);
+
+            // Initialize managers
+            pieChartManager = new PieChartManager(pieChart, requireContext());
+            barChartManager = new BarChartManager(barChart, trendToggle, requireContext());
+
+//            updateValues(1000f,600f);
+        } catch (Exception e) {
+            Log.e("ReportsFragment", "Error initializing views or managers", e);
+        }
+
+        return view;
+    }
+    private void updateValues(float newIncome, float newExpense) {
+        try {
+            income = newIncome;
+            expense = newExpense;
+            float balance = income - expense;
+
+            tvTotalIncome.setText(String.format(requireContext().getString(R.string.currency_format), income));
+            tvTotalExpense.setText(String.format(requireContext().getString(R.string.currency_format), expense));
+            tvBalanceAmount.setText(String.format(requireContext().getString(R.string.currency_format), balance));
+
+            pieChartManager.updateChart(income, expense);
+            barChartManager.updateBarChart(true);
+        } catch (Exception e) {
+            Log.e("ReportsFragment", "Error updating values", e);
+        }
     }
 }
