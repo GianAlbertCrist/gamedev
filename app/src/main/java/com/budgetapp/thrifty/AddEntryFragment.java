2359f3dc02b531ade29f932c1893baf15c2ab6ac
Gian Daytic <s.daytic.gianalbert@cmu.edu.ph>
2025-05-03 16:24:04 +0800

    Added Something

---
+++ app/src/main/java/com/budgetapp/thrifty/AddEntryFragment.java
@@ -0,0 +1,15 @@
+package com.budgetapp.thrifty;
+
+import android.os.Bundle;
+import androidx.fragment.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class AddEntryFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_add_entry, container, false);
+    }
+}
