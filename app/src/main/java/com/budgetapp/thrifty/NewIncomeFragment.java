7a8b140864e44c0200dd09667c4fbb152748560d
Gian Daytic <s.daytic.gianalbert@cmu.edu.ph>
2025-05-04 00:43:34 +0800

    Added Scrollview

---
+++ app/src/main/java/com/budgetapp/thrifty/NewIncomeFragment.java
@@ -0,0 +1,19 @@
+package com.budgetapp.thrifty;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class NewIncomeFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_new_income, container, false);
+    }
+}
