136e7375e0d9cb013f64117a5f4ee90610903fab
Gian Daytic <s.daytic.gianalbert@cmu.edu.ph>
2025-05-08 04:34:01 +0800

    Added Firebase Realtime Database

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,6 +46,8 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+    implementation(platform("com.google.firebase:firebase-bom:33.13.0"))
+    implementation("com.google.firebase:firebase-analytics")
     implementation("com.github.PhilJay:MPAndroidChart:v3.1.0")
     implementation("com.google.android.material:material:1.5.0")
 }
