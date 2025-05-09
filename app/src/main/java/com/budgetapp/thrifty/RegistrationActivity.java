24b1ced6d8ab1869099ba372469f144d67b44998
Gian Daytic <s.daytic.gianalbert@cmu.edu.ph>
2025-05-09 21:51:29 +0800

    Naa ayo na error sa registration

--- app/src/main/java/com/budgetapp/thrifty/RegistrationActivity.java
+++ app/src/main/java/com/budgetapp/thrifty/RegistrationActivity.java
@@ -12,7 +12,6 @@ import android.view.View;
 import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import com.google.android.material.textfield.TextInputEditText;
