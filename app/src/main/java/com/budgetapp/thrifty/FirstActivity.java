1c4331cac877fd61cb28cd8380ad0471b2f2b54b
Gian Daytic <s.daytic.gianalbert@cmu.edu.ph>
2025-05-10 16:03:00 +0800

    Add the manifest.xml

--- app/src/main/java/com/budgetapp/thrifty/FirstActivity.java
+++ app/src/main/java/com/budgetapp/thrifty/FirstActivity.java
@@ -21,11 +21,12 @@ public class FirstActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first);
 
-
         ThemeSync.syncNotificationBarColor(getWindow(), this);
+
         // Initialize Firebase Auth
         mAuth = FirebaseAuth.getInstance();
 
+        // Find views using correct type (Button)
         signInButton = findViewById(R.id.sign_in_button);
         registerButton = findViewById(R.id.register_button);
 
@@ -43,10 +44,8 @@ public class FirstActivity extends AppCompatActivity {
     @Override
     public void onStart() {
         super.onStart();
-        // Check if user is signed in (non-null) and update UI accordingly.
         FirebaseUser currentUser = mAuth.getCurrentUser();
         if (currentUser != null) {
-            // User is already signed in, go to MainActivity
             Intent intent = new Intent(FirstActivity.this, MainActivity.class);
             startActivity(intent);
             finish();
